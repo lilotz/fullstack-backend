a814d0236c8c720a30b877f26767450e35b00270
lisalotz <lisa.lotz@tu-dortmund.de>
2025-10-04 15:49:16 +0300

    exercise 3.21

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -21,7 +21,9 @@ const errorHandler = (error, request, response, next) => {
     if (error.name === 'CastError') {
         response.status(400).send({ error: 'malformatted id' })
     }
-
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
     next(error)
 }
 
@@ -64,37 +66,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const { name, number } = request.body
-
-    if (!name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-
-    if (!number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-
-    /*const names = entries.map(person => person.name)
-
-    if (names.some(name => name === body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }*/
 
     const entry = new Entry({
         name: name,
         number: number
     })
 
-    entry.save().then(savedEntry => {
-        response.json(savedEntry)
-    })
+    entry.save()
+        .then(savedEntry => {
+            response.json(savedEntry)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
--- part3/phonebook/models/entry.js
+++ part3/phonebook/models/entry.js
@@ -15,8 +15,21 @@ mongoose.connect(url)
     })
 
 const entrySchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v)
+                 && v.length > 7
+            }
+        },
+        required: true
+    },
 })
 
 entrySchema.set('toJSON', {
