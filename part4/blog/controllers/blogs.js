9c77ee245783b60e28ac2988b8457bec7d763a85
lisalotz <lisa.lotz@tu-dortmund.de>
2025-10-09 11:14:31 +0300

    exercise 4.8

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -7,13 +7,11 @@ blogsRouter.get('/', (request, response, next) => {
     .catch(error => next(error))
 })
 
-blogsRouter.post('/', (request, response, next) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-    .catch(error => next(error))
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
 
 module.exports = blogsRouter
